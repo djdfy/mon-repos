a33867e512f4f4ed344de9e818575fd5121855c9
@djdfy <vtsarev@enecuum.com>
2023-10-07 19:46:26 +0300

    validParentheses

--- validParentheses.js
+++ validParentheses.js
@@ -1,36 +1,27 @@
-let s = "{}{}{{{[]}}}{}";
+
+let s = "((([]){}))]";
 
 function isValidParentheses(s) {
-	let closedMap = new Map([
-			[')','('],
-			[']','['],
-			['}','{']
-		]);
-	let i = 0;
-	let L = 0;
-	while(i < s.length) {
-		if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
-          	if (s[i+1]) {
-              	i++;
-				L++; 
-            } else
-            	return false;        
-		} else if (s[i] !== undefined) {
-        	if (L == 0)
-            	return false
-          	let counter = 1;
-        	for(let j=i; j <  i+L; j++) {
-              	if (s[j] == undefined || closedMap.get(s[j]) !== s[j-counter])                  
-					return false;
-              	counter = counter + 2;
-			}
-          	i += L;
-          	L = 0;
-        } else
-        	return false;
-	}
 
-	return true;
+    let closedMap = new Map([
+                ['(',')'],
+                ['[',']'],
+                ['{','}']
+            ]);
+  let stack = [];  
+  
+  for (let symbol of s) {
+    if (closedMap.has(symbol)) {
+    	stack.push(symbol);    
+    } else if (!closedMap.has(symbol)) {
+      let a = stack.pop();
+      if (!a || closedMap.get(a) !== symbol)
+          return false      
+    }
+  }
+
+ return stack.length === 0;
 }
+
 
 console.log(isValidParentheses(s))
